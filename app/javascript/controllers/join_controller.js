4ee4019d64b3c3bdb0a230b89367651a7d6c3167
Aaron <mail@aapomm.io>
2024-12-21 12:53:39 +0800

    CSS changes

--- app/javascript/controllers/join_controller.js
+++ app/javascript/controllers/join_controller.js
@@ -1,21 +1,29 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = [ "create", "code" ]
-
-  initialize() {
-    this.codeTarget.hidden = true
-  }
+  static targets = [ "create", "code", "cancel", "help" ]
 
   join() {
-    if (this.codeTarget.hidden) {
-      this.codeTarget.hidden = false
+    if (this.codeTarget.classList.contains('hidden')) {
+      this.codeTarget.classList.remove('hidden')
       this.codeTarget.focus()
-      this.createTarget.hidden = true
+
+      this.cancelTarget.classList.remove('hidden')
+
+      this.createTarget.classList.add('hidden')
+      this.helpTarget.classList.add('hidden')
     }
     else {
       window.location.href = `/games/${this.codeTarget.value}`
     }
+  }
+
+  cancel() {
+    this.codeTarget.classList.add('hidden')
+    this.cancelTarget.classList.add('hidden')
+
+    this.createTarget.classList.remove('hidden')
+    this.helpTarget.classList.remove('hidden')
   }
 
   upcase() {
